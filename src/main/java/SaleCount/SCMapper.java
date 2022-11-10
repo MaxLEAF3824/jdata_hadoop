010fdeab6ad90390a806de21222be980603226ab
MaxLEAF3824 <guoyiqiu_max@foxmail.com>
2022-11-10 23:05:00 +0800

    更新统计销量的代码

--- src/main/java/SaleCount/SCMapper.java
+++ src/main/java/SaleCount/SCMapper.java
@@ -18,7 +18,7 @@ public class SCMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
             return;
         String time = split[2];
         // 用正则表达式匹配出时间中的年月日时分，格式为:2016-03-12 00:08:36
-        time = time.replaceAll("^(\\d{4})-(\\d{2})-(\\d{2}) (\\d{2}):(\\d{2}):(\\d{2})$", "$1-$2-$3 $4:$5:$6");
+        time = time.replaceAll("^(\\d{4})-(\\d{2})-(\\d{2}) (\\d{2}):(\\d{2}):(\\d{2})$", "$1-$2-$3 $4:$5");
         outKey.set(time);
         context.write(outKey, one);
     }
