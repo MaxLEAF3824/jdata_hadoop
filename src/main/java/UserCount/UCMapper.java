91df684943bc9339a9d4e362c424dd6d59cd1287
MaxLEAF3824 <guoyiqiu_max@foxmail.com>
2022-11-10 18:44:22 +0800

    更新第三问

--- src/main/java/UserCount/UCMapper.java
+++ src/main/java/UserCount/UCMapper.java
@@ -15,9 +15,9 @@ public class UCMapper extends Mapper<LongWritable, Text, Text, Text> {
         String[] split = line.split(",");
         String userId = split[0];
         String time = split[2];
-        // 用正则表达式匹配出时间中的年月日，格式为:2016-03-12 00:08:36
-        String date = time.replaceAll("^(\\d{4})-(\\d{2})-(\\d{2})\\s.*$", "$1-$2-$3");
-        outKey.set(date);
+        // 用正则表达式匹配出时间中的年月日时分，格式为:2016-03-12 00:08:36
+        time = time.replaceAll("^(\\d{4})-(\\d{2})-(\\d{2}) (\\d{2}):(\\d{2}):(\\d{2})$", "$1-$2-$3 $4");
+        outKey.set(time);
         outValue.set(userId);
         context.write(outKey, outValue);
     }
